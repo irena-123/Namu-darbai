7267b77354b9daecf2428fd5ec2f11358e398ff5
irena-123 <irena.kalinauskaite@gmail.com>
2019-10-11 13:49:51 +0300

    pirmas commitas

---
+++ src/IrenaND.java
@@ -0,0 +1,9 @@
+public class IrenaND {
+    public static void main(String args[]) {
+      String firstname = "Irena";
+      String lastname = "Kalinauskaite";
+      String jobrole = "Business Analyst";
+      System.out.println("Hello, my name is " + firstname + " " + lastname + ".");
+      System.out.println("I'm a " + jobrole + ".");
+    }
+}
